e01cd57bde5c25e062c3d4c80c3de94102b4045e
creezed <glamber3@mail.ru>
2026-05-11 00:39:20 +0300

    feat(crm): create CRM context skeleton with four Nx libraries

---
+++ libs/backend/crm/interfaces/jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+  displayName: 'backend-crm-interfaces',
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  preset: '../../../../jest.preset.cjs',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+};
