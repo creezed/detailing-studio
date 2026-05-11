9f8a11e0747aaa5a2f3e32f64c870706a18288aa
creezed <glamber3@mail.ru>
2026-05-11 22:57:27 +0300

    chore(work-order): scaffold application, infrastructure, interfaces libs

---
+++ libs/backend/work-order/interfaces/jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+  displayName: 'work-order-interfaces',
+  preset: '../../../../jest.preset.cjs',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  testMatch: ['**/*.e2e.ts'],
+};
