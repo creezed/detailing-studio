d1315a919955f50a0f4b53d95c933c7032fe9fb3
creezed <glamber3@mail.ru>
2026-05-15 03:09:27 +0300

    refactor(infra): move scheduler app to apps/backend/scheduler

--- apps/backend/scheduler/jest.config.ts
+++ apps/backend/scheduler/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
   displayName: 'scheduler',
-  preset: '../../jest.preset.cjs',
+  preset: '../../../jest.preset.cjs',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/scheduler',
+  coverageDirectory: '../../../coverage/apps/backend/scheduler',
 };
