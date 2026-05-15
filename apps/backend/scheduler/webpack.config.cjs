4ab093f9c00c6afb8a11db1c104a974eaae9bccf
creezed <glamber3@mail.ru>
2026-05-15 03:09:27 +0300

    refactor(infra): move scheduler app to apps/backend/scheduler

--- apps/backend/scheduler/webpack.config.cjs
+++ apps/backend/scheduler/webpack.config.cjs
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../dist/apps/scheduler'),
+    path: join(__dirname, '../../../dist/apps/backend/scheduler'),
   },
   plugins: [
     new NxAppWebpackPlugin({
